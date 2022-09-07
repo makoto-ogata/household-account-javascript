754738a650b0c868ccd3e13a9c5150176a175c8c
Makoto Ogata <ogatamakoto1976@gmail.com>
2022-09-08 02:12:30 +0900

    create add datebase method

--- indexedDB.js
+++ indexedDB.js
@@ -27,16 +27,16 @@ database.onerror = function (event) {
 }
 
 // フォームの内容をDBに登録する
-function regist(){
+function regist() {
     // フォームの入力チェック。falseが返却されたら登録処理を中断
-    if(inputCheck() === false) {
+    if (inputCheck() === false) {
         return;
     }
     // ラジオボタンの取得
     let radio = document.getElementsByName('balance');
     let balance;
-    for(let i = 0; i < radio.length; i++) {
-        if(radio[i].checked === true) {
+    for (let i = 0; i < radio.length; i++) {
+        if (radio[i].checked === true) {
             balance = radio[i].value;
             break;
         }
@@ -48,58 +48,106 @@ function regist(){
     let memo = document.getElementById('memo').value;
     let category = document.getElementById('category').value;
     // ラジオボタンが収入を選択時はカテゴリを「収入」とする
-    if(balance === '収入') {
+    if (balance === '収入') {
         category = '収入';
     }
 
     //データベースにデータを登録する
     insertData(balance, date, category, amount, memo);
 
-    function insertData(balance, date, category, amount, memo){
-        // 一意のIDを現在の日時から作成
-        let uniqueID = new Date().getTime().toString();
-        console.log(uniqueID);
-        // DBに登録するための連想配列のデータを作成
-        let data = {
-            id: uniqueID,
-            balance: balance,
-            date: String(date),
-            category: category,
-            amount: amount,
-            memo: memo,
-        }
-
-        // データベースを開く
-        let database = indexedDB.open(dbName, dbVersion);
-
-        // データベースを開けなかった時の処理
-        database.onerror = function (event){
-            console.log('データベースに接続できませんでした');
-        }
-
-        // データベースを開いたらデータの登録を実行
-        database.onsuccess = function (event){
-            let db = event.target.result;
-            let transaction = db.transaction(storeName, 'readwrite');
-            transaction.complete = function(event){
-                console.log('トランザクション完了');
-            }
-
-            transaction.onerror = function(event){
-                console.log('トランザクションエラー');
-            }
-
-            let store = transaction.objectStore(storeName);
-            let addData = store.add(data);
-            addData.onsuccess = function() {
-                console.log('データが登録できました');
-                alert('登録しました');
-            }
-            addData.onerror = function (){
-                console.log('データが登録できませんでした')
-            }
-            db.close();
-        }
+    // 入手金一覧を作成
+    createList();
+}
+function insertData(balance, date, category, amount, memo){
+    // 一意のIDを現在の日時から作成
+    let uniqueID = new Date().getTime().toString();
+    console.log(uniqueID);
+    // DBに登録するための連想配列のデータを作成
+    let data = {
+        id: uniqueID,
+        balance: balance,
+        date: String(date),
+        category: category,
+        amount: amount,
+        memo: memo,
     }
+
+    // データベースを開く
+    let database = indexedDB.open(dbName, dbVersion);
+
+    // データベースを開けなかった時の処理
+    database.onerror = function (event){
+        console.log('データベースに接続できませんでした');
+    }
+
+    // データベースを開いたらデータの登録を実行
+    database.onsuccess = function (event){
+        let db = event.target.result;
+        let transaction = db.transaction(storeName, 'readwrite');
+        transaction.complete = function(event){
+            console.log('トランザクション完了');
+        }
+
+        transaction.onerror = function(event){
+            console.log('トランザクションエラー');
+        }
+
+        let store = transaction.objectStore(storeName);
+        let addData = store.add(data);
+        addData.onsuccess = function() {
+            console.log('データが登録できました');
+            alert('登録しました');
+        }
+        addData.onerror = function (){
+            console.log('データが登録できませんでした')
+        }
+        db.close();
+    }
+}
+
+function createList() {
+    // データベースからデータを全件取得
+    let database = indexedDB.open(dbName);
+    database.onsuccess = function(event){
+        let db = event.target.result;
+        let transaction = db.transaction(storeName, 'readonly');
+        let store = transaction.objectStore(storeName);
+        store.getAll().onsuccess = function (data) {
+            console.log(data);
+            let rows = data.target.result;
+
+            let section = document.getElementById('list');
+            // 入出金一覧のテーブルを作る
+            // バッククオートでヒアドキュメント
+            let table = `
+                <table>
+                    <tr>
+                        <th>日付</th>
+                        <th>収支</th>
+                        <th>カテゴリ</th>
+                        <th>金額</th>
+                        <th>メモ</th>
+                        <th>削除</th>
+                    </tr>
+            `;
+
+            // 入出金のデータを表示
+            rows.forEach((element) => {
+                console.log(element);
+                table += `
+                <tr>
+                    <td>${element.date}</td>
+                    <td>${element.balance}</td>
+                    <td>${element.category}</td>
+                    <td>${element.amount}</td>
+                    <td>${element.memo}</td>
+                    <td><button>✕</button></td>
+                </tr>
+               `;
+                });
+                table += `</table>`;
+                section.innerHTML = table;
+            }
+        }
 }
 
